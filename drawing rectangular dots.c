977bc0231892edbe024d7428f8504b2eaff5b6fe
TranPhucVinh <tranphucvinh96@gmail.com>
2017-08-30 01:05:34 -0500

    drawing rectangular dots

---
+++ drawing rectangular dots.c
@@ -0,0 +1,47 @@
+/* drawing 
+
+
+   m 
+********
+*      *   n   
+*	   *     
+********
+
+*/
+
+#include <stdio.h>
+
+main() {
+
+	int n,m;
+
+	printf("enter m " );
+	scanf("%d",&m);
+	printf("enter n");
+	scanf("%d",&n);
+	for (int i=1; i<=n ; i++)
+	{
+		if ((i==1) || (i==n) )
+			{ 
+for (int j=1; j<=m; j++)
+{
+printf("*");	
+}
+printf("\n");
+			}
+			else {
+				for (int j =1; j<=m; j++)
+				{
+					if ( (j==1 )|| (j==m))
+					{
+  					printf("*");
+					}
+					else{
+						printf(" ");
+					}
+				}
+				printf("\n");
+			}
+	}
+
+} 
