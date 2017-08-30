83e9a7b85c97aede0a8311dc074a2d656644216e
TranPhucVinh <tranphucvinh96@gmail.com>
2017-08-30 03:37:04 -0500

    reverse number

--- char.c
+++ char.c
@@ -3,7 +3,7 @@
 main() {
 
 	// enter a char, print out this char => learn how to use getchar(); putchar()
-	
+
 char a;
 a = getchar();
 
---
+++ reverse number.c
@@ -0,0 +1,19 @@
+// enter a number, print it in reverse order: 12345 => 54321
+
+#include <stdio.h>
+
+main () {
+
+	int a,b, number=0;
+
+	printf("enter the value of a");
+	scanf("%d",&a);
+
+	while(a != 0)
+	{
+		b = a%10;
+		a = a/10;
+		number = number*10 + b;
+	}
+	printf("%d \n", number );
+}
