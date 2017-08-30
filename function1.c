f35a33e892c1141e3d48972350a4348c063c7a7e
TranPhucVinh <tranphucvinh96@gmail.com>
2017-08-30 13:45:55 -0500

    aaa

--- function1.c
+++ function1.c
@@ -19,7 +19,7 @@ double fracture (int n)
 
 	for (double i = 1.0; i<=n; i++)
 	{
- m = m +(1/i);
+ m = m +(1/i); // if you declare int i =1 in for loop, the result of (1/i) will be kept as int
 	}
 	return m;
 }
