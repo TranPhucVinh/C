3fc4b03412bc3ecfdafa3f7fd68378d7a0cf77d3
TranPhucVinh <tranphucvinh96@gmail.com>
2017-08-30 02:23:33 -0500

    find prime number

--- char.c
+++ char.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 main() {
+
+	// enter a char, print out this char => learn how to use getchar(); putchar()
+	
 char a;
 a = getchar();
 
---
+++ prime number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+main () {
+
+	int n, count=0;
+	printf(" enter the limit value " );
+	scanf("%d",&n);
+
+	if (n<=0)
+		{ 
+printf("shitty");
+		}
+		else
+		{
+			for (int i=1; i<=n ; i++)
+			{
+              for (int j=1; j<=i; j++)
+              {
+              	if ((i%j) ==0)
+              	{
+              		count++;
+              	}
+              }
+              if (count == 2) {
+              	printf("%d ",i );
+              }
+              count =0;
+			}
+		}
+}
+
+//result:
+// on terminal
+// enter the limit value 29
+// 2 3 5 7 11 13 17 19 23 29
