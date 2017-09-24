a1fb78c353e969e8a31f50c146d6f3e4273a451d
TranPhucVinh <tranphucvinh96@gmail.com>
2017-09-24 10:19:54 -0500

    expert

--- expert-system.c
+++ expert-system.c
@@ -1,41 +1,58 @@
 // tư vấn học ngôn ngữ lập trình
 #include <stdio.h>
-#include <string.h>
+
 main () 
 { int n;
 printf(" BẠN THEO HƯƠNG LAP TRÌNH NÀO ? \n " );
-printf(" (Nhập vào số tương ứng với định hướng bạn chọn) \n
-1. Web \n
-2. Desktop \n
-3. Mobile \n
-4. Embedded \n
-5. Khác \n" );
+printf(" (Nhập vào số tương ứng với định hướng bạn chọn) \n\
+1. Web \n\
+2. Desktop \n\
+3. Mobile \n\
+4. Embedded \n\
+5. Khác \n " );
 
 
 scanf("%d",&n);
 int a;
-char s1;
+char s0, s1;
 
 switch (n)
 {
 	case 1:
 	printf(" các hướng lập trình web \n
-	       1. Thiết kế web \n
-	       2. Server \n
-	       bạn chọn hướng nào ? (Nhập vào số tương ứng \n" );
+		(Nhập vào số tương ứng) \n
+	       1. Front End \n
+	       2. Back End: App \n
+	       3. Back End: Database \n
+	       bạn chọn hướng nào ?  \n" );
 	scanf("%d",&a);
 	if (a == 1)
 	{
-    printf( " Đầu tiên học HTML, sau đó học CSS, JavaScript, JQuerry \n" );
-    printf ("Bạn nên ưu tiên học JavaScript để có thể vừa lập trình giao diện và server. Đồng thời có thể test trực tiếp giao diện bằng localhost với JavaScript" );
-
+    printf( " Bạn cần ưu tiên học theo thứ tự sau: \n
+    1/ HTML: Thiết kế giao diện web. Bạn cũng có thể học XHTML, cũng có khả năng như HTML như có cấu trúc chặt chẽ hơn HTML \n 
+    2/ CSS : Ngôn ngữ để định dạng trang web \n
+    3/ JavaScript: Là ngôn ngữ lập trình cho HTML và Web. JavaScript hỗ trợ tạo các web động và hỗ trợ phía client của web tương tác \n
+    với người dùng. Đoạn code JavaScript sẽ được chèn vào phần script của đoạn code HTML \n
+    Bấm một phím bất kì để tiếp tục \n" );
+    s1 = getchar();
+    if ( (s0 >= 'a' && s0 <= 'z') || (s0 >= '0' && s0 <= '9'))
+    {
+    	printf("Bạn chỉ quan tâm tới Front end thôi sao ?" );
+    }
 	}
-	else 
-	{
-	printf(" bạn cần server cho các ứng dụng real-time không ? \n" );
-	printf("có (y) \n" );
-	printf("không (n) \n");
+ WORK0:
+ 	else 
+ {
+	printf(" bạn cần server cho các ứng dụng real-time không ? \n
+	         có (y) \n
+	         không (n) \n");
 	s1 = getchar();
+	if (s1 = 'y')
+	{
+	printf("học Nodejs"); 	
+	}
+ }
+ break;
 	// so sánh s1
 	// nếu đúng
 	// printf (" học Node JS (JavaScript) ")
@@ -47,21 +64,29 @@ switch (n)
 	// nếu sai: 
 	case 2:
 	printf("on going \n" ); 
+	break;
 	case 3:
-	printf("Bạn cần lập trình cho hệ điều hành nào ? (Chọn số thứ tự tương ứng) \n" );
-    printf ("1. Android" );
-    printf ("2. IOS" );
-    printf ("3. Windows Phone" );
+	printf("Bạn cần lập trình cho hệ điều hành nào ? (Chọn số thứ tự tương ứng) \n
+          1. Android \n
+          2. IOS \n
+          3. Windows Phone" );
     scanf();
+    break;
     case 4:
-    printf (" Mỗi vi điều khiển có một ngôn ngữ riêng. Bạn cần học 3 ngôn ngữ C, C++ để làm nền tảng (Đây là 2 ngôn ngữ thông dụng nhất");
-    printf(" Bạn cũng có thể học thêm ngôn ngữ Assembly" );
+    printf (" Mỗi vi điều khiển có một ngôn ngữ riêng. Bạn cần học 2 ngôn ngữ C, C++ để làm nền tảng (Đây là 2 ngôn ngữ thông dụng nhất \n
+              Bạn cũng có thể học thêm ngôn ngữ Assembly" );
+    break;
     case 5:
+    printf("Có thể bạn quan tâm tới các hướng sau: \n
+    	1. Internet of Things \n
+    	2. Bảo mật - An ninh mạng \n
+    	Nếu đúng, hãy nhập vào số tương ứng ở trên, nếu không, hãy nhấn phím "n" trên bàn phím ");
+    break;
         
-	}
+}	
 
 }
 
 
-}
+
  
