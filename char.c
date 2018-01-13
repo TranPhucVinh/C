3360330729cd7befb96b5099f38ad20d00a1155f
TranPhucVinh <tranphucvinh96@gmail.com>
2018-01-14 00:36:41 +0700

    reident

--- char.c
+++ char.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
+#include < stdio.h >
 
-main() {
+  main() {
 
-	// enter a char, print out this char => learn how to use getchar(); putchar()
+    // enter a char, print out this char => learn how to use getchar(); putchar()
 
-char a,b[1],c[1];
-a = getchar(); // get ASCII of the entered character
+    char a, b[1], c[1];
+    a = getchar(); // get ASCII of the entered character
 
-putchar(a); // putchar is displaying the char on screen
-printf("\n");
+    putchar(a); // putchar is displaying the char on screen
+    printf("\n");
 
-//we will find out if the number entered is lowercase or uppercase
-if (a =='y')
-{
-	printf("kill bugs" );
-}
-else
-{
-	printf("nothing here \n" );
-}
- // from here, you need to distinguished the different between "" and ''. '' is for character, " " is for string
+    //we will find out if the number entered is lowercase or uppercase
+    if (a == 'y') {
+      printf("kill bugs");
+    } else {
+      printf("nothing here \n");
+    }
+    // from here, you need to distinguished the different between "" and ''. '' is for character, " " is for string
 
-// now we learn how to deal with entering the char inside if...else
+    // now we learn how to deal with entering the char inside if...else
 
-scanf("%s",b);
-printf("%s \n",b );
-if ( (strcmp(b,"t")) ==0  ) // must be "t", not 't'
-{
-	scanf("%s",c);
-printf("%s \n",c );
-printf("roc \n");
-}
-else {
-	printf("fuck the system \n" );
-}
+    scanf("%s", b);
+    printf("%s \n", b);
+    if ((strcmp(b, "t")) == 0) // must be "t", not 't'
+    {
+      scanf("%s", c);
+      printf("%s \n", c);
+      printf("roc \n");
+    } else {
+      printf("fuck the system \n");
+    }
 
-}
+  }
 
 //result :
 /*
--- compare 2 numbers.c
+++ compare 2 numbers.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#include < stdio.h >
+  // compare 2 numbers and print out the square of number from 1 to n
+  main() {
+    int n;,
+    printf(" enter your number: ");
+    scanf("%d", & n); // if you type scanf("&d ", &n) => C compile require you to press any
+    // keys after entering the number n value
 
-// compare 2 numbers and print out the square of number from 1 to n
-
-main() {
-
-	int n;, 
-
-	printf(" enter your number: ");
-
-	scanf("%d", &n); // if you type scanf("&d ", &n) => C compile require you to press any
-	// keys after entering the number n value
-	
-	for (int i =0; i <= n; i++)
-		{ int a = i*i;
-printf("%d \n", a );
-		}
-}
+    for (int i = 0; i <= n; i++) {
+      int a = i * i;
+      printf("%d \n", a);
+    }
+  }
