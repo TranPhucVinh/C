d19f2228d4055c56040e79f07bc7a88f7ffde0a8
TranPhucVinh <tranphucvinh96@gmail.com>
2018-01-14 22:59:58 +0700

    add files

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -8,5 +8,15 @@ printf("%p ", &a);
 
 // print out the address of variable a: 0x7ffee13d1fa4 
 
-printf(a);
+int *p;
+
+p = a;
+printf("%d ", p); // result: 5
+
+p = &a;
+printf("%d \n", p); // result: 39367436 (address of variable - pointer p)
+
+*p = a;
+printf("%d ",p ); // result: 39367436 
+
 }
