f15a0e0c4d8accaa2fcc85ada9232d1a4e0f82c3
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2018-01-14 22:42:39 +0700

    Update compare 2 numbers.c

--- compare 2 numbers.c
+++ compare 2 numbers.c
@@ -3,7 +3,7 @@
   main() {
     int n;
     printf(" enter your number: ");
-    scanf("%d", & n); // if you type scanf("&d ", &n) => C compile require you to press any
+    scanf("%d", &n); // if you type scanf("&d ", &n) => C compile require you to press any
     // keys after entering the number n value
 
     for (int i = 0; i <= n; i++) {
