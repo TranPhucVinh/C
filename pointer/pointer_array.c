3c46280801e13099122faf047fb110938132b08b
TranPhucVinh <tranphucvinh96@gmail.com>
2018-02-11 19:23:39 +0700

    add pointer array

---
+++ pointer/pointer_array.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+void main() {
+	int n, *p1, *p2, i;
+	int a[20];
+	printf(" Number of members in array: ");
+	scanf("%d", &n);
+	p1 = p2 = &a[0];
+	printf("Enter the value of every member: " );
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", p1);
+		p1++;
+	}
+	printf("Value of all members in array: ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *p2 );
+		p2++;
+	}
+}
