4b030fcfc3fd5a065fcb58b123b5536d8020c3a8
TranPhucVinh <tranphucvinh96@gmail.com>
2018-02-13 17:08:14 +0700

    add working with files

---
+++ Working with files/open.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main() {
+FILE *file;
+file = fopen("file","rt+");
+}
---
+++ Working with files/readFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+	FILE *stream;
+	char msg[81];
+	stream = fopen("open.c","r");
+	fgets(msg, 81, stream);
+	printf("%s\n",msg );
+	fclose(stream); // without fclose() still run fine
+}
