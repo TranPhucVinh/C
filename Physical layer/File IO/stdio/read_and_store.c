36c3a261f506e71170f566ba16bc0dfc851fe248
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-01-07 19:43:05 +0700

    WIP: detect issue when reading line by line

--- Physical layer/File IO/stdio/read_and_store.c
+++ Physical layer/File IO/stdio/read_and_store.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
-#define MEMBER      3
-#define STRING_SIZE 12
+#define ELEMENT_NUMBERS 1
+#define SIZE_OF_LINE 30
 
-char stringArray[MEMBER][STRING_SIZE];
-char filePath[] = "json.txt";
+int total_line;
+int get_file_total_line(char *file_name);
 
-int index;
-FILE *fptr; //file pointer
+int main(int argc, char *argv[]) {
+	total_line = get_file_total_line("test.c");
+	//Can only define the string array here as total line must be const value
+	char string_array[total_line][SIZE_OF_LINE];
 
-int main(){
-   fptr = fopen(filePath, "r");
-
-   while (index < MEMBER){
-      if (fgets(stringArray[index], STRING_SIZE, fptr) == NULL) break;
+	FILE *fptr = fopen("test.c", "r");
+	int index = 0;
+	while (index < total_line){
+      if (fgets(string_array[index], SIZE_OF_LINE, fptr) == NULL) break;
       else {
-         //Remove the \n character
-         if(stringArray[index][STRING_SIZE-2] == '\n') stringArray[index][STRING_SIZE-2] ='\0';
-         index++;
+		//Remove the \n character
+		if(string_array[index][SIZE_OF_LINE-2] == '\n') string_array[index][SIZE_OF_LINE-2] ='\0';
+		index++;
       }   
    }
-   for (int i=0; i < MEMBER; i++){
-		printf("stringArray[%i]: %s \n", i, stringArray[i]);
+   for (int i=0; i < total_line; i++){
+		printf("stringArray[%i]: %s", i, string_array[i]);
 	}
+}	
+
+int get_file_total_line(char *file_name){
+	long file_size;
+	FILE *fp;
+	fp = fopen(file_name, "r");
+	if (fp){
+		fseek(fp, 0L, SEEK_END);
+		file_size = ftell(fp);
+
+		fseek(fp, 0L, SEEK_SET);
+
+		char *buffer;
+		buffer = (char *) malloc(file_size);
+		bzero(buffer, file_size);
+		fread(buffer, file_size, ELEMENT_NUMBERS, fp);
+		
+		int total_line = 0;
+		for (int i = 0; i < file_size; i++){
+			if (buffer[i] == '\n') total_line += 1;
+		}
+		fclose(fp);
+		return total_line+1;
+	} else printf("Unable to open file %s\n", file_name);
+	return 0;
 }
