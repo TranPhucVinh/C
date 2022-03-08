a5c1a2f0a7e3562bfb80d812f30811e2e9d90124
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-03-08 17:35:05 +0700

    Get access token successfully

--- Platforms interaction/ThingsBoard/thingsboard_get_access_token.c
+++ Platforms interaction/ThingsBoard/thingsboard_get_access_token.c
@@ -16,13 +16,16 @@
 #define CUSTOMER_ID_SIZE	40
 #define TOTAL_DEVICE_ID		10
 
+#define ACCESS_TOKEN		20
+
 char token[TOKEN_SIZE], refreshToken[REFRESH_TOKEN_SIZE];
 char *device_id_array[TOTAL_DEVICE_ID];
 
 char *http_response = NULL;
 char *get_customer_id(char *http_response, int customer_id_size);
 char *get_all_customer_devices(char *customer_id);
-int  get_device_id(char *json_string, char *field_name);
+int  get_device_id(char *json_string, char *field_name, char *device_id_array[TOTAL_DEVICE_ID]);
+void get_access_token(char *device_id_array[TOTAL_DEVICE_ID], int total_devices);
 
 int main(int argc, char *argv[]){
 	server_side_api_param_init(HOST, PORT, LOGIN_PATH, USER, PASSWORD);
@@ -43,11 +46,9 @@ int main(int argc, char *argv[]){
 	customer_devices_json = strstr(customer_devices_info, "{\"data\":");
 
 	//Get all existed devices ID
-	int total_device = get_device_id(customer_devices_json, "data");
+	int total_devices = get_device_id(customer_devices_json, "data", device_id_array);
 
-	for (int i=0; i < total_device; i++){
-		printf("%s\n", device_id_array[i]);
-	}
+	get_access_token(device_id_array, total_devices);
 
 	free(http_response);
 	free(customer_id);
@@ -142,7 +143,7 @@ char *get_all_customer_devices(char *customer_id){
 }
 
 //return total devices that a customer have
-int get_device_id(char *json_string, char *field_name){
+int  get_device_id(char *json_string, char *field_name, char *device_id_array[TOTAL_DEVICE_ID]){
 	const cJSON *array_field = NULL;
 	cJSON *json = cJSON_Parse(json_string);
 	cJSON *array_member = NULL;
@@ -165,10 +166,85 @@ int get_device_id(char *json_string, char *field_name){
 
 			char *device_id_string = cJSON_Print(device_id);
 
+			//Remove " character from device_id_string at index 0 and index strlen(device_id_string)-1
+			memmove(device_id_string, device_id_string+1, strlen(device_id_string));
+			device_id_string[strlen(device_id_string)-1] = 0;
+			
 			device_id_array[index] = device_id_string;
-			// printf("member %d: %s\n", index, device_id_string);
+			// printf("%s\n", device_id_string);
 			index += 1;
 		}
 	} else printf("Fail");
 	return index;
+}
+
+#define GET_ACCESS_TOKEN "/api/device/"
+
+void get_access_token(char *device_id_array[TOTAL_DEVICE_ID], int total_devices){
+	for (int index=0; index < total_devices; index++){
+		char    *response_buffer;
+		int 	fd;
+
+		response_buffer = (char*) malloc(BUFFSIZE * sizeof(char));
+		bzero(response_buffer, BUFFSIZE);
+
+		char http_request[1024];//Must be big to store token
+		bzero(http_request, sizeof(http_request));
+
+		strcat(http_request, "GET ");
+		strcat(http_request, GET_ACCESS_TOKEN);
+		strcat(http_request, device_id_array[index]);
+		strcat(http_request, "/credentials");	
+		strcat(http_request, " HTTP/1.1\r\nHost: ");
+		strcat(http_request, HOST);
+		strcat(http_request, "\r\nContent-Type: application/json");
+		strcat(http_request, "\r\nX-Authorization: Bearer ");
+		strcat(http_request, token);
+		strcat(http_request, "\r\nConnection: close\r\n\r\n");
+
+		fd 		= socket_connect();
+		write(fd, http_request, strlen(http_request));
+
+		//After reading all HTTP response from JWT (not until reaching BUFFSIZE character), break the while loop
+		//Without break, the program will hang as read() will keep waiting for the coming bytes although there is no bytes left
+		while(read(fd, response_buffer, BUFFSIZE) != 0) {
+			break;
+		}
+
+		shutdown(fd, SHUT_RDWR); 
+		close(fd);
+
+		char* temp_json_parsed = NULL;
+		temp_json_parsed = strstr(response_buffer, "credentialsId");
+
+		// printf("%s\n", temp_json_parsed);
+
+		int credentials_id_size = strlen(temp_json_parsed) + 1;//1 for "{"
+
+		char credentials_id[credentials_id_size];
+		bzero(credentials_id, credentials_id_size);
+
+		strcat(credentials_id, "{\"");
+		strcat(credentials_id, temp_json_parsed);
+	
+		cJSON *json_object = cJSON_Parse(credentials_id);
+		if (json_object == NULL)
+		{
+			const char *error_ptr = cJSON_GetErrorPtr();
+			if (error_ptr != NULL)
+			{
+				fprintf(stderr, "Error before: %s\n", error_ptr);
+			}
+		}
+
+		char *access_token = (char*) malloc(ACCESS_TOKEN * sizeof(char));
+
+		cJSON *credentials_id_object = cJSON_GetObjectItemCaseSensitive(json_object, "credentialsId");
+
+		if (cJSON_IsString(credentials_id_object) && (credentials_id_object->valuestring != NULL))
+		{
+			strcpy(access_token, credentials_id_object->valuestring);
+			printf("Access token %s\n", access_token);
+		} else printf("Fail to get token");
+	}
 }
