ba3fba59487cf37eceb67b1c9a1e1177ca493c64
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-05-21 01:26:49 +0700

    Update program to the latest thingsboard lib version

--- Platforms interaction/ThingsBoard/thingsboard_auth_user.c
+++ Platforms interaction/ThingsBoard/thingsboard_auth_user.c
@@ -9,7 +9,6 @@
 #define USER        "tranphucvinh96@gmail.com"
 #define PASSWORD    "iotdev_admin"
 
-#define BUFFSIZE        	4096 //Size must be big to get all JWT
 #define TOKEN_SIZE			600
 #define REFRESH_TOKEN_SIZE	500 
 
@@ -18,16 +17,16 @@ char token[TOKEN_SIZE], refreshToken[REFRESH_TOKEN_SIZE];
 char *http_response = NULL;
 
 int main(int argc, char *argv[]){
-	_server_side_api_param_init(HOST, PORT, LOGIN_PATH, USER, PASSWORD);
+	server_side_api_param_init(HOST, PORT, LOGIN_PATH, USER, PASSWORD);
 
 	bzero(token, TOKEN_SIZE);
 	bzero(refreshToken, REFRESH_TOKEN_SIZE);
 
-	get_jwt(token, refreshToken, BUFFSIZE);
+	get_jwt(token, refreshToken);
 	// printf("debug token: %s\n", token);
 	// printf("debug refreshToken: %s\n", refreshToken);
 
-	http_response = http_request_for_api_with_jwt(API, "GET", token, BUFFSIZE);
+	http_response = http_request_for_api_with_jwt(API, "GET", token);
 	printf("%s\n", http_response);
 	free(http_response);
 
