a7b21af100f02f95e7a09c21a8d59d9c3642217b
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-06-21 07:29:02 +0700

    Update source codes for character device and user space ioctl

---
+++ Kernel/Character device/character_device_ioctl.c
@@ -0,0 +1,100 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME					"fops_character_device"
+#define DEVICE_CLASS				"fops_device_class"
+
+MODULE_LICENSE("GPL");
+
+dev_t dev_id;
+
+struct cdev *character_device;
+struct class *device_class;
+struct device *device;
+
+int dev_open(struct inode *, struct file *);
+int dev_close(struct inode *, struct file *);
+ssize_t dev_read(struct file*, char __user *, size_t, loff_t *);
+ssize_t dev_write(struct file *, const char __user *, size_t, loff_t *);
+long dev_ioctl(struct file *, unsigned int cmd, unsigned long arg);
+
+struct file_operations fops = {
+	.open = dev_open,
+	.release = dev_close,
+	.read = dev_read,
+	.write = dev_write,
+	.unlocked_ioctl = dev_ioctl,
+};
+
+int dev_open(struct inode *inodep, struct file *filep)
+{
+	printk("open\n");
+	return 0;
+}
+
+int dev_close(struct inode *inodep, struct file *filep)
+{
+	printk("close\n");
+	return 0;
+}
+
+ssize_t dev_read(struct file*filep, char __user *buf, size_t len, loff_t *offset)
+{
+	printk("read\n");
+	return 0;
+}
+
+char data[100];
+ssize_t dev_write(struct file *filep, const char __user *buf, size_t len, loff_t *offset)
+{
+	memset(data, 0, sizeof(data));
+	copy_from_user(data, buf, len);
+	printk("write data: %s\n", data);
+	return sizeof(data);
+}
+
+long dev_ioctl(struct file *filep, unsigned int cmd, unsigned long arg){
+	int user_space_argument;
+	copy_from_user(&user_space_argument, (int*)arg, sizeof(user_space_argument));
+	printk("cmd %d, arg %d, user_space_argument %d\n", cmd, arg, user_space_argument);
+
+	return 0;
+}
+
+int device_init(void)
+{
+	int ret;
+
+	ret = alloc_chrdev_region(&dev_id, 0, 1, "fops_alloc_chrdev_region");
+	if(ret)
+	{
+		printk("can not register major no\n");
+		return ret;
+	}
+	printk(KERN_INFO "register successfully major now is: %d\n", MAJOR(dev_id));
+	character_device = cdev_alloc();
+	character_device->owner = THIS_MODULE;
+	character_device->ops = &fops;
+	character_device->dev = dev_id;
+
+	cdev_add(character_device, dev_id, 1);
+	device_class = class_create(THIS_MODULE, DEVICE_CLASS);
+	device = device_create(device_class, NULL, dev_id, NULL, DEVICE_NAME);
+
+	return 0;
+}
+
+void device_exit(void)
+{
+	printk("Device remove\n");
+	unregister_chrdev_region(dev_id, 0);
+	cdev_del(character_device);
+	device_destroy(device_class, dev_id);
+	class_destroy(device_class);
+}
+
+module_init(device_init);
+module_exit(device_exit);
---
+++ Kernel/Character device/user_space_ioctl.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> //for open()
+#include <sys/ioctl.h>
+
+#define CHAR_DEV    "/dev/fops_character_device"
+
+int fd;
+int cmd = 10;
+int argument = 123;
+
+int main(int argc, char *argv[]) {
+	fd = open(CHAR_DEV, O_RDWR);
+	if (fd){
+		if (ioctl(fd, cmd, &argument) == -1) {
+            printf("Fail\n");
+        } else {
+            printf("cmd %d, argument %d\n", cmd, argument);
+        }
+	}
+}	
