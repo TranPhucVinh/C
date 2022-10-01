417f34636c23be999165e6f8e33cbc45b6431a95
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2022-10-01 19:24:51 +0700

    Implement linked list on heap memory

--- Kernel/Linked list/create_and_read_linked_list.c
+++ Kernel/Linked list/create_and_read_linked_list.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
+#include <linux/slab.h> //for kmalloc()
 
 #define TOTAL_NODES	3
 
@@ -11,7 +12,8 @@ struct data_base_node {
     int value;
 };
 
-struct data_base_node node_0, node_1, node_2;
+typedef struct data_base_node data_base_node;
+data_base_node *node_0, *node_1, *node_2;
 
 void display_list(struct data_base_node first_node){
     struct list_head *list_ptr = &(first_node.list);
@@ -29,21 +31,28 @@ void display_list(struct data_base_node first_node){
 
 int init_module(void)
 {
-	node_0.value = 1;
-	node_1.value = 2;
-	node_2.value = 3;
+    node_0 = (data_base_node *) kmalloc(sizeof(data_base_node), GFP_KERNEL);
+    node_1 = (data_base_node *) kmalloc(sizeof(data_base_node), GFP_KERNEL);
+    node_2 = (data_base_node *) kmalloc(sizeof(data_base_node), GFP_KERNEL);
 
-	INIT_LIST_HEAD(&node_0.list);
+	node_0->value = 1;
+	node_1->value = 2;
+	node_2->value = 3;
 
-	list_add(&node_1.list, &node_0.list);
-	list_add(&node_2.list, &node_1.list);
+	INIT_LIST_HEAD(&(node_0->list));
+
+	list_add(&(node_1->list), &(node_0->list));
+	list_add(&(node_2->list), &(node_1->list));
 	printk(KERN_INFO "Setup link list\n");
 
-	display_list(node_0);
+	display_list(*node_0);
 	return 0;
 }
 
 void cleanup_module(void)
 {
+    kfree(node_0);
+    kfree(node_1);
+    kfree(node_2);
 	printk("Clean up module\n");	
 }
