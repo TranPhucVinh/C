353d7a62fd9260d7c9736efe0c101b824572b9b2
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-10-03 00:00:25 -0400

    Code formatting to underscore, perform operation from first_node argument

--- Introduction/Data structure/Linked list/Doubly Linked list/create.c
+++ Introduction/Data structure/Linked list/Doubly Linked list/create.c
@@ -1,83 +1,84 @@
 #include <stdio.h> 
 #include <stdlib.h>
-#include <string.h>
 
-struct databaseNode { 
+struct database_node { 
     int id;
-    int intValue;
-    struct databaseNode* nextNode; 
-    struct databaseNode* previousNode; 
-} *node0; 
+    int value;
+    struct database_node* next_node; 
+    struct database_node* previous_node; 
+}; 
 
 //Insert value to Next node and link 2 nodes to each other
-void insertNodeValues(struct databaseNode *previousNode, struct databaseNode *currrentNode, struct databaseNode *nextNode, int id, int intValue)  {
-    currrentNode->id = id;
-    currrentNode->intValue = intValue;
-    currrentNode->previousNode = previousNode;
-    currrentNode->nextNode = nextNode;
+void insert_new_node(struct database_node *previous_node, struct database_node *current_node, struct database_node *next_node, int id, int value){
+    current_node->id = id;
+    current_node->value = value;
+    current_node->previous_node = previous_node;
+    current_node->next_node = next_node;
 }
 
-void displayLinkList(){
-    struct databaseNode *ptr = node0;
+void display_linked_list(struct database_node *first_node){
+    struct database_node *ptr = first_node;
 
     while(ptr != NULL)
 	{        
-      printf("(%d,%d)", ptr->id, ptr->intValue);
-      ptr = ptr->nextNode;
+        printf("(%d,%d)", ptr->id, ptr->value);
+        ptr = ptr->next_node;
     }
-    puts("");
+    printf("\n");
 }
 
-void readDataFromPreviousNode(struct databaseNode *node){
+void read_previous_node_data(struct database_node *node){
     //Exception: first node
-    if (node->previousNode == NULL) {
-        puts("This is the first node");
+    if (node->previous_node == NULL) {
+        printf("This is the first node\n");
         return;
     }
 
     //Get ID of previous node from current node
-    printf("node->previousNode->id: %d \n", node->previousNode->id); 
+    printf("node->previous_node->id: %d \n", node->previous_node->id); 
 
-    //Get intValue of previous node from current node
-    printf("node1->previousNode->intValue: %d \n", node->previousNode->intValue);
+    //Get value of previous node from current node
+    printf("node->previous_node->value: %d \n", node->previous_node->value);
 }
 
-void readDataFromNextNode(struct databaseNode *node){
+void read_next_node_data(struct database_node *node){
     //Exception: End node
-    if (node->nextNode == NULL) {
+    if (node->next_node == NULL) {
         puts("This is the end node");
         return;
     }
     //Get ID of next node from current node
-    printf("node->nextNode->id: %d \n", node->nextNode->id); 
+    printf("node->next_node->id: %d\n", node->next_node->id); 
 
-    //Get intValue of next node from current node
-    printf("node->nextNode->intValue: %d \n", node->nextNode->intValue);
+    //Get value of next node from current node
+    printf("node->next_node->value: %d\n", node->next_node->value);
 }
 
-void readDataFromCurrentNode(struct databaseNode *node){
+void read_current_node_data(struct database_node *node){
     //Get ID of current node
     printf("node->id: %d \n", node->id); 
 
-    //Get intValue of current node
-    printf("node1->intValue: %d \n", node->intValue);
+    //Get value of current node
+    printf("node->value: %d \n", node->value);
 }
 
 int main(int argc, char *argv[])
 { 
-    node0 = (struct databaseNode *)malloc(sizeof(struct databaseNode));
+	struct database_node *node_0 = (struct database_node *)malloc(sizeof(struct database_node));
 
-    struct databaseNode node1, node2;
-    insertNodeValues(NULL, node0, &node1, 1, 100);
-    insertNodeValues(node0, &node1, &node2, 2, 200);
-    insertNodeValues(&node1, &node2, NULL, 3, 300);
+    struct database_node *node_1 = (struct database_node *)malloc(sizeof(struct database_node));
+	struct database_node *node_2 = (struct database_node *)malloc(sizeof(struct database_node));
 
-    displayLinkList();
+    insert_new_node(NULL, node_0, node_1, 1, 100);
+    insert_new_node(node_0, node_1, node_2, 2, 200);
+    insert_new_node(node_1, node_2, NULL, 3, 300);
+
+    display_linked_list(node_0);
 
     //Read data from previous node, current node and next node to check the integrity of the link list
-    readDataFromPreviousNode(node0);
-    readDataFromCurrentNode(node0);
-    readDataFromNextNode(node0);
+    read_previous_node_data(node_1);
+    read_current_node_data(node_1);
+    read_next_node_data(node_1);
 
     return 0; 
 } 
