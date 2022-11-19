030ffe6ee496e2d36e6f2c3f41b0019fd46df39c
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-11-19 20:56:14 +0700

    Update program to latest feature descriptions

--- Application layer/TCP/tcp_client.c
+++ Application layer/TCP/tcp_client.c
@@ -1,9 +1,10 @@
-#include <stdio.h>      /* for printf() and fprintf() */
+#include <stdio.h>      
 #include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
 #include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
+#include <errno.h>
 
 #define HOST "127.0.0.1"
 #define PORT 8000
@@ -25,13 +26,17 @@ void socket_parameter_init(){
 
     // Create TCP socket
     if ((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
-        error("socket() failed");
-    } else printf("create TCP socket success \n");
+        printf("Fail to create socket\n");
+        exit(0);
+    } else printf("Create socket successfully\n");
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0){
-        error("ERROR connecting");
-    } else printf("connect to server success \n");
+        if (errno == ECONNREFUSED) {
+            printf("Server hasn't been started or server doesn't support connection\n");
+        } else printf("Can't connect to server with error %d", errno);
+        exit(0);
+    } else printf("connect to server success\n");
 }
 
 int main(){
