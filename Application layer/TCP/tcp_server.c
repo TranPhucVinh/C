172aa0574c7e68acb682580d24998957ee63d439
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-11-25 19:39:39 +0700

    Update tcp_server.c

--- Application layer/TCP/tcp_server.c
+++ Application layer/TCP/tcp_server.c
@@ -1,15 +1,23 @@
 #include <stdio.h>          
-#include <sys/socket.h>     /* for socket(), connect(), send() and recv() */
+#include <sys/socket.h>     /* for socket(), connect()*/
 #include <stdlib.h>         /* for atoi() and exit() */
 #include <string.h>         /* for memset() */
 #include <unistd.h>         /* for close() */
 #include <sys/types.h> 
 #include <netinet/in.h>
 #include <arpa/inet.h>      /* for inet_ntop() */
+#include <signal.h> 
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 #define MAXPENDING 	5
 #define BUFFSIZE 	256
 #define PORT 		8000
+
+//Macro for mmap() setup
+#define PAGE_SIZE   4048
+#define NO_FD       -1  //No file descriptor used for shared memory
+#define BASE_ADDR   0   //No specific base address to set in shared memory
 
 int 		server_fd, client_fd;
 socklen_t 	clntLen;
@@ -17,9 +25,12 @@ char 		buffer[BUFFSIZE];
 struct 		sockaddr_in serv_addr, cli_addr;
 
 int         socket_id = 0;
-int         total_connected_client = 0;
+int         *total_connected_client;
 
 void socket_parameter_init(){
+    total_connected_client = (int *)mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, NO_FD, BASE_ADDR);
+    *total_connected_client = 0;
+
     //Create socket server
     if ((server_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
         perror("tcp_server\n");
@@ -55,20 +66,29 @@ int main(int argc, char *argv[]){
             char ip_str[30];
             // now get it back and print it
             inet_ntop(AF_INET, &(cli_addr.sin_addr.s_addr), ip_str, INET_ADDRSTRLEN);
-            total_connected_client += 1;
-            printf("New tcp client connected with IP %s, %d TCP clients have connected\n", ip_str, total_connected_client);
+            *total_connected_client += 1;
+            socket_id += 1;
+            printf("New tcp client connected with IP %s, %d TCP clients have connected now\n", ip_str, *total_connected_client);
 
             int pid = fork();
-            if (!pid) {
-                socket_id += 1;
+            if (!pid) { 
+                printf("TCP client has ID %d\n", socket_id);
                 while (1){
-                    int bytes_received = recv(client_fd, buffer, BUFFSIZE, 0);
-                    if (bytes_received) {
+                    int bytes_received = read(client_fd, buffer, BUFFSIZE);
+                    if (bytes_received > 0) {
                         printf("Message from TCP client ID %d: %s", socket_id, buffer);
                         bzero(buffer, BUFFSIZE);         //Delete buffer
+                    } 
+                    /*
+                        Right after the TCP socket is disconnected, read() will return 0.
+                        If tcp_client.c sends empty string, read() won't return 0. 
+                    */
+                    else if (bytes_received == 0) {
+                        printf("TCP client with ID %d is disconnected\n", socket_id);
+                        *total_connected_client -= 1;
+                        printf("%d TCP clients have connected now\n", *total_connected_client);
+                        kill(getpid(), SIGKILL);
                     }
-                    // n = read(client_fd, buffer, 1024);
-                    // printf("%s\n", buffer);
                 }
             } 
         }
