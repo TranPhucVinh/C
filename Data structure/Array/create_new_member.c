dcb00ee84bbdf0a5ceb56c13f8a31a06f728643c
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2023-03-23 19:03:16 +0700

    Fix program, add precondition

--- Data structure/Array/create_new_member.c
+++ Data structure/Array/create_new_member.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 
-int array[] = {1,3,5,7,8};
-int addIndex = 2;
-int newMemberValue = 10;
-int i, j;
-size_t arraySize = sizeof(array)/sizeof(array[0]);
+/*
+    Allocate array with size bigger than its current total member
+    for inserting
+*/
+int array[100] = {1,3,5,7,8};
+int array_size = 5;
 
-void arrayBeforeAdding(){
-  printf("Array before adding: ");
-	
-  for(i = 0; i < arraySize; i++) {
-    printf("%d ", array[i]);
-  }
-  printf("\n");
+//Display array before inserting
+void display_array(int arr[], int arr_size){
+    for(int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
-void arrayAfterAdding(){
-  arraySize++;
+void insert_member(int arr[], int *arr_size, int insert_index, int insert_val){
+    int j = *arr_size;
+    while(j >= insert_index){
+        array[j+1] = array[j];
+        j--;
+    }
 	
-  while(j >= addIndex){
-    array[j+1] = array[j];
-    j--;
-  }
-	
-  array[addIndex] = newMemberValue;
-   
-  printf("Result after adding: ");
-	
-  for(i = 0; i < arraySize; i++) {
-    printf("%d ", array[i]);
-  }
-  printf("\n");
+    array[insert_index] = insert_val;
+    *arr_size += 1;
 }
 
 int main() {   
-  arrayBeforeAdding();
-  arrayAfterAdding();
+    printf("Array before adding: ");
+    display_array(array, array_size);
+    insert_member(array, &array_size, 2, 10);
+    printf("Array after inserting: ");
+    display_array(array, array_size);
 }
