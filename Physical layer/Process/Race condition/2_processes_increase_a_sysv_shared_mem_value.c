d3dc7f7889a77fa3e26d4dd466e05266aa5903e5
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2023-09-14 14:20:22 +0700

    Two processes increase the value of a System V shared memory region

--- Physical layer/Process/Race condition/2_processes_increase_a_sysv_shared_mem_value.c
+++ Physical layer/Process/Race condition/2_processes_increase_a_sysv_shared_mem_value.c
@@ -1,73 +1,43 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/shm.h>
-#include <sys/mman.h>
-#include <fcntl.h>  
+#include <fcntl.h> 
 #include <errno.h>
+#include <sys/shm.h>
 #include <semaphore.h>
-#include <unistd.h>
 
-#define SIZE    100// Size of the share memory segment
+#define SHM_KEY 1 // Shared memory SHM_KEY must exist at first
+#define SIZE    50// Size of the share memory segment
 
-#define SHM_ADDR         NULL
-#define SHARED_MEM_PATH    "/dev/shm/SHARED_MEMORY" 
+#define SHM_ADDR    NULL
+#define SEM_VAL	    1
+
+#define RANGE   1000000
+// #define RANGE   5 // Small RANGE value like this won't cause race condition
+
 #define SEM_NAME    "/SEMAPHORE_SHARED_MEM"
-
-#define DUMMY_PROJECT_ID   1
+#define PSHARED     1
 
 int id;
 int *ptr;
-sem_t *sem;
-
+sem_t *mutex;
 int main(){
-    sem = sem_open(SEM_NAME, O_RDWR, 777, 1);
-    if (sem == SEM_FAILED) {
-        printf("Fail to open %s\n", SEM_NAME);
+    id = shmget(SHM_KEY, SIZE, 0777);
+    ptr = shmat(id, SHM_ADDR, SHM_EXEC);
+
+    mutex = sem_open(SEM_NAME, O_RDWR);
+
+    if (mutex == SEM_FAILED) {
+        printf("Fail open %s\n", SEM_NAME);
         printf("errno %d\n", errno);
-        if (errno == 13) printf("Permission denied\n");
-        return 0;
+        if (errno = 13) printf("Permission denied\n");
     }
     else printf("Open %s successfully\n", SEM_NAME);
 
-    key_t shm_key = ftok(SHARED_MEM_PATH, DUMMY_PROJECT_ID);
-
-    if (shm_key == -1) {
-        printf("error ftok the shared mem: %d\n", errno);
-        return 0;
-    }
-    printf("ftok() success\n");
-    id = shmget(shm_key, SIZE, 0777|IPC_CREAT);
-    if (id > -1) printf("Shared memory segment from shm_key %d with ID %d is got successfully\n", shm_key, id);
-    else {
-        printf("Unable to get shared memory segment share memory key %d\n", id);
-        printf("errno %d\n", errno);
-        return 0;
-    }
-
-    ptr = shmat(id, SHM_ADDR, SHM_EXEC|0777);
-
-    if (*ptr == -1) printf("Shared memory segment is unable to be attached\n");
-    else {
-        printf("Shared memory segment is attached successfully with value %d\n", *ptr);
-    }
-
-    if(!sem_wait(sem)) {
-        sem_post(sem);
-    }
-
-    sleep(1);
-
-    #define RANGE 1000000
-    for (int i = 0; i < RANGE; i++) {
-        if(!sem_wait(sem)) {
+    for (int i = 0; i < RANGE; i++){
+        if (!sem_wait(mutex)){
             *ptr += 1;
-            sem_post(sem);
-        } else printf("fail to lock\n");
+            sem_post(mutex);
+        } else printf("%d fails to lock\n", *ptr);
     }
-    printf("fail %d\n", *ptr);
-
-    if (shmdt(ptr) < 0) {
-        printf("detach share mem fail: %d\n", errno);
-    }
-
+    
+    printf("%d\n", *ptr);
 }
