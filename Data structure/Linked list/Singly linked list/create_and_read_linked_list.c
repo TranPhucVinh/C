d2dbf07b721b77cee29986e66b7af90403062f83
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-02-07 22:58:02 +0700

    Create and read linked list on heap memory

--- Data structure/Linked list/Singly linked list/create_and_read_linked_list.c
+++ Data structure/Linked list/Singly linked list/create_and_read_linked_list.c
@@ -1,21 +1,23 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-struct database_node { 
+struct _LinkedList { 
     int id;
     int value;
-    struct database_node* next_node; 
+    struct _LinkedList* next_node; 
 }; 
 
+typedef struct _LinkedList LinkedList;
+
 //Insert value to Next node and link 2 nodes to each other
-void insert_new_node(struct database_node *current_node, struct database_node *next_node, int id, int value){
+void insert_new_node(LinkedList *current_node, LinkedList *next_node, int id, int value){
     next_node->id = id;
     next_node->value = value;
     current_node->next_node = next_node;
 }
 
-void display_link_list(struct database_node *first_node){
-    struct database_node *ptr = first_node;
+void display_link_list(LinkedList *first_node){
+    LinkedList *ptr = first_node;
 
     while(ptr != NULL)
 	{        
@@ -27,14 +29,14 @@ void display_link_list(struct database_node *first_node){
 
 int main() 
 { 
-	struct database_node *node_0, *node_1, *node_2;
-	node_0 = (struct database_node *)malloc(sizeof(struct database_node));
+    LinkedList *node_0, *node_1, *node_2;
+	node_0 = (LinkedList *)malloc(sizeof(LinkedList));
 
     node_0->id = 1;
     node_0->value = 100;
 
-    node_1 = (struct database_node *)malloc(sizeof(struct database_node));
-    node_2 = (struct database_node *)malloc(sizeof(struct database_node));
+    node_1 = (LinkedList *)malloc(sizeof(LinkedList));
+    node_2 = (LinkedList *)malloc(sizeof(LinkedList));
     insert_new_node(node_0, node_1, 2, 200);
     insert_new_node(node_1, node_2, 3, 300);
     node_2->next_node = NULL; //End link list
