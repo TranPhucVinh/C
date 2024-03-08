9d2150c05fe89229754e7ffd6c87345e9f98bb46
Tran Phuc Vinh <tranphucvinh96@gmail.com>
2024-03-08 21:16:48 +0700

    Linear search compared to binary search

--- Algorithms/src/Hanoi_tower.c
+++ Algorithms/src/Hanoi_tower.c
@@ -2,7 +2,7 @@
 
 void moveDisk(int diskNumber, int startPos, int midPos, int endPos);
 
-main(){
+int main(){
 	int diskNumber;
 	printf("Enter disk number: ");
 	scanf("%d", &diskNumber);
@@ -13,11 +13,11 @@ void moveDisk(int diskNumber, int startPos, int midPos, int endPos)
 {
 	if (diskNumber >1)
 	{		// move n-1 upper disk from startPos to midPos
-			moveDisk(diskNumber-1, startPos, endPos, midPos);
-			//move the biggest disk from startPos to endPos
-			printf("move disk number %d from column %d to column %d \n", diskNumber, startPos, endPos);
-			// move n-1 upper disk from midPos to endPos
-			moveDisk(diskNumber-1, midPos, startPos, endPos);
+		moveDisk(diskNumber-1, startPos, endPos, midPos);
+		//move the biggest disk from startPos to endPos
+		printf("move disk number %d from column %d to column %d \n", diskNumber, startPos, endPos);
+		// move n-1 upper disk from midPos to endPos
+		moveDisk(diskNumber-1, midPos, startPos, endPos);
 	}
-		else printf("move disk number 1 from column %d to column %d \n", startPos, endPos);
+	else printf("move disk number 1 from column %d to column %d \n", startPos, endPos);
 }
