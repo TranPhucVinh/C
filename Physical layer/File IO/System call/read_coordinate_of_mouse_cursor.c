7c62c466fc0837035194635f99774e16f1dafea8
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-03-16 13:01:57 +0700

    Read mouse coordinate: struct input_event with value with signed type

--- Physical layer/File IO/System call/read_coordinate_of_mouse_cursor.c
+++ Physical layer/File IO/System call/read_coordinate_of_mouse_cursor.c
@@ -12,7 +12,7 @@ struct input_event {
     struct timeval time;
     unsigned short type;
     unsigned short code;
-    unsigned int value;
+    signed value;
 };
 
 void main(void)
@@ -20,7 +20,7 @@ void main(void)
     int fd, fsize;
     char buff[128];
     struct input_event *temp;
-    unsigned int x, y;
+    signed x, y;
     x = y = 0;
     fd = open(MOUSE_INPUT_EVENT, O_RDONLY);
     if (fd >0){
@@ -33,7 +33,7 @@ void main(void)
                 if (temp->code == REL_X) x = temp->value;// X-axis
                 else if (temp->code == REL_Y) y = temp->value;// Y-axis
             }
-            printf("x=%u y=%u\n", x, y);
+            printf("x=%d y=%d\n", x, y);
         }
     } else printf("%d\n", fd);
 }
