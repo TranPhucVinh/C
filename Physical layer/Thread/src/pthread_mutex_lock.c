c67f8c1386c0220602982122520e249219cefb1c
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-03-25 10:51:48 +0700

    pthread_mutex_lock(): One thread function handler to increase a shared value

--- Physical layer/Thread/src/pthread_mutex_lock.c
+++ Physical layer/Thread/src/pthread_mutex_lock.c
@@ -21,7 +21,7 @@ int main()
 	pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
     pthread_join(thread_3, NULL);
-    printf("share_value after executing 2 threads: %d\n", share_value);//9000000
+    printf("share_value after executing 3 threads: %d\n", share_value);//9000000
 	pthread_mutex_destroy(&lock);
 }
 
