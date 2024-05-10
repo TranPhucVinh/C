4095f2f8008f361f0577f1943e9be2077069a726
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-05-10 22:28:19 +0700

    Recursive mutex
    2 threads count down from a number with thread_odd only prints odd numbers and thread_even only prints even numbers, and both of those threads are defined recursively, a recursive mutex is mandatorily used.

---
+++ Physical layer/Thread/src/recursive_mutex_threads_print_odd_and_even_numbers.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUMBER  7
+
+void *odd_number(void *ptr);
+void *even_number(void *ptr);
+
+int number = NUMBER;
+
+pthread_mutex_t lock;
+
+int main() {
+    pthread_mutexattr_t recursiveMutexAttr;
+    pthread_mutexattr_init(&recursiveMutexAttr);
+    pthread_mutexattr_settype(&recursiveMutexAttr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&lock, &recursiveMutexAttr);
+
+    // pthread_mutex_init(&lock, NULL); -> This will cause deadlock
+
+    pthread_t thread_odd, thread_even;
+    int thread_odd_return, thread_even_return;
+
+    thread_odd_return = pthread_create(&thread_odd, NULL, odd_number, &number);
+    thread_even_return = pthread_create(&thread_even, NULL, even_number, &number);
+
+    pthread_join(thread_odd, NULL);
+    pthread_join(thread_even, NULL);
+
+    pthread_mutex_destroy(&lock);
+    return 0;
+}
+
+void *odd_number(void *ptr) {
+    if(!pthread_mutex_lock(&lock)){
+        if ( *(int*)ptr > 0) {
+            printf("Odd number: %d\n", *(int*)ptr);
+
+            *(int*)ptr -= 1;
+            even_number(ptr);           
+		} 
+        pthread_mutex_unlock(&lock);// With recursive mutex, it must be unlocked after using the shared resource 
+    } else printf("Fail to lock mutex\n");
+}
+
+void *even_number(void *ptr) {
+    if(!pthread_mutex_lock(&lock)){
+        if ( *(int*)ptr > 0) {
+            printf("Even number: %d\n", *(int*)ptr);
+            
+            *(int*)ptr -= 1;
+            pthread_mutex_unlock(&lock);
+            odd_number(ptr);
+        } 
+        pthread_mutex_unlock(&lock);// With recursive mutex, it must be unlocked after using the shared resource 
+    } else printf("Fail to lock mutex\n");
+}
