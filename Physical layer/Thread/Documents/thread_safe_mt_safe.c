60e77d3aa64e0145b8273123071cec21df3bc220
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-05-17 11:44:35 +0700

    Thread safe MT-safe

---
+++ Physical layer/Thread/Documents/thread_safe_mt_safe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define ARR_SZ      5
+#define RANGE       100000000
+#define MUTEX_ATTR  NULL // Mutex attribute
+
+void *thread_function(void *ptr);
+
+pthread_mutex_t lock[ARR_SZ];
+
+int array[ARR_SZ];
+
+int main() {  
+    for (int i = 0; i < ARR_SZ; i++){
+        pthread_mutex_init(&lock[i], MUTEX_ATTR);
+    }
+
+	pthread_t thread_1, thread_2;
+	int thread1_return, thread2_return;
+
+	thread1_return = pthread_create(&thread_1, NULL, thread_function, NULL);
+    thread2_return = pthread_create(&thread_2, NULL, thread_function, NULL);
+
+	pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+
+    for (int i = 0; i < ARR_SZ; i++){
+       printf("%d ", array[i]);
+    }
+}
+
+void *thread_function(void *ptr) {
+    for (int i = 0; i < ARR_SZ; i++){
+        if(!pthread_mutex_lock(&lock[i])){
+            for (int j = 0; j < RANGE; j++) {            
+                array[i]++;
+            }
+            pthread_mutex_unlock(&lock[i]);    
+        } else printf("Mutex fails to lock\n"); 
+    }
+    return NULL;
+}
