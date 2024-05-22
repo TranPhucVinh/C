44f9f2d6b2b2a872b1c7a41e5b2cf8a66769c632
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-05-22 17:01:13 +0700

    Get host information by getaddrinfo()

---
+++ Application layer/HTTP client/TCP socket/get_host_info_getaddrinfo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define HOST "example.com"
+#define PORT "80"
+
+int main(){
+    struct addrinfo hints = {
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM,
+	};
+
+    struct addrinfo *response;
+
+    int err = getaddrinfo(HOST, PORT, &hints, &response);
+    if(err != 0 || response == NULL) {
+		printf("DNS lookup failed err=%d res=%p", err, response);
+		return -1;
+	}
+
+    struct in_addr *addr;
+    addr = &((struct sockaddr_in *)response->ai_addr)->sin_addr;
+    printf("DNS lookup success, IP: %s\n", inet_ntoa(*addr));// 93.184.215.14
+
+	return 0;
+}
