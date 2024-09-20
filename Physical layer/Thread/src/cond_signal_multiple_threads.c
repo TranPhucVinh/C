02139158b3c8babda2eb0c8dec01f5c14d5f40e4
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2024-09-20 22:45:57 +0700

    Run multiple threads respectively by condition variables
    Must include mutex and pthread_cond_wait()

--- Physical layer/Thread/src/cond_signal_multiple_threads.c
+++ Physical layer/Thread/src/cond_signal_multiple_threads.c
@@ -12,10 +12,14 @@ void *func_thread_3(void *ptr);
 
 bool thread_2_run = false, thread_3_run = false;
 
+pthread_mutex_t lock;
 pthread_cond_t thread_2_run_condition, thread_3_run_condition;
 
 int main() {  
 	pthread_t thread_1, thread_2, thread_3;
+
+    pthread_mutex_init(&lock, MUTEX_ATTR);
+    pthread_cond_init(&thread_2_run_condition, NULL);
 
 	pthread_create(&thread_1, NULL, func_thread_1, NULL);
     pthread_create(&thread_2, NULL, func_thread_2, NULL);
@@ -27,34 +31,44 @@ int main() {
 }
 
 void *func_thread_1(void *ptr){    
-    for (int i = 0; i < RANGE; i++) {
-        printf("Thead 1: %d\n", i);
+    if(!pthread_mutex_lock(&lock)){
+        for (int i = 0; i < RANGE; i++) {
+            printf("Thead 1: %d\n", i);
+        }
+        pthread_mutex_unlock(&lock);
+        thread_2_run = true;
+        pthread_cond_signal(&thread_2_run_condition);
+        sleep(1);
     }
-    thread_2_run = true;
-    pthread_cond_signal(&thread_2_run_condition);
-    sleep(1);
 }
 
 void *func_thread_2(void *ptr){
-    while(!thread_2_run){
-        printf("Thread 2 isn't ready to run\n");
-        sleep(1);   
+    if(!pthread_mutex_lock(&lock)){
+        while(!thread_2_run){
+            printf("Thread 2 isn't ready to run\n");
+            pthread_cond_wait(&thread_2_run_condition, &lock); 
+        }
+        for (int i = 0; i < RANGE; i++) {
+            printf("Thead 2: %d\n", i);
+        }
+        pthread_mutex_unlock(&lock);
+        thread_3_run = true;
+        pthread_cond_signal(&thread_3_run_condition);
+        sleep(1);
     }
-    for (int i = 0; i < RANGE; i++) {
-        printf("Thead 2: %d\n", i);
-    }
-    thread_3_run = true;
-    pthread_cond_signal(&thread_3_run_condition);
 }
 
 void *func_thread_3(void *ptr) {
-    while(!thread_3_run){
-        printf("Thread 3 isn't ready to run\n");
-        sleep(1);   
-    }
+    if(!pthread_mutex_lock(&lock)){
+        while(!thread_3_run){
+            printf("Thread 3 isn't ready to run\n");
+            pthread_cond_wait(&thread_3_run_condition, &lock);    
+        }
 
-    for (int i = 0; i < RANGE; i++) {
-        printf("Thead 3: %d\n", i);
+        pthread_mutex_unlock(&lock);
+        for (int i = 0; i < RANGE; i++) {
+            printf("Thead 3: %d\n", i);
+        }
+        sleep(1);
     }
-    sleep(1);
 }
